dec91790b55ac8e5ec0f2ce03f6d07828a1675c0
mrccdrc27 <mayuga.marccedricc@gmail.com>
2025-05-15 00:49:44 +0800

    merge from lawrence sent UI

--- frontend/src/components/navigations/admin-nav/AdminNav.jsx
+++ frontend/src/components/navigations/admin-nav/AdminNav.jsx
@@ -1,11 +1,10 @@
-import {NavLink} from 'react-router-dom'
-import { useState } from 'react'; 
+import { NavLink, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'; 
 
 import styles from "./admin-nav.module.css";
 import DateBanner from '../datetime-banner/DateBanner';
-
-
 export default function AdminNav() {
+  const location = useLocation();
 
   // pa remove nalang if di need
     const handleAvatarClick = () => {
@@ -20,27 +19,45 @@ export default function AdminNav() {
 
     // pa remove nalang if di need
 
+    useEffect(() => {
+        const handleResize = () => {
+          setUserMenuOpen(false);
+          setMenuOpen(false)
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        // clean event listener
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []); 
 
-      // state for menu bar
-    const [menuOpen, setMenuOpen] = useState(false); 
-
+    // Burger Menu
+    const [menuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => {
-      setMenuOpen(!menuOpen); 
+      setMenuOpen(!menuOpen);
+      setUserMenuOpen(false);
     };
+
+    // Three dots Menu
+    const [userMenuOpen, setUserMenuOpen] = useState(false); 
+    const toggleUserMenu = () => {
+      setUserMenuOpen(prev => !prev);
+      setMenuOpen(false);
+      setOpenProfileModal(false); 
+      setOpenNotifModal(false);
+    };  
 
   return(
     <>
         <nav className={styles.navBar}> 
-      <div className={styles.logoSection}> 
-        <div className={styles.logoImg}></div>
-        <span>DTS</span>
-      </div>
-
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-      </div>
+          <div className={styles.logoSection}> 
+            <div className={styles.logoImg}>
+              <img src="/logotixx.png" alt="logo" />
+            </div>
+            <p>TicketFlow</p>
+          </div>
 
       {/* nav-links */}
       <div className={`${styles.navLinks} ${menuOpen ? styles.active : ''}`}>
@@ -59,13 +76,29 @@ export default function AdminNav() {
           className={({isActive}) => isActive ? `${styles.navLink} ${styles.active}` : styles.navLink}>Archive</NavLink>
       </div>
 
-      <div className={styles.userSection}>
-        <p>Username</p>
+      <div className={`${styles.userSection} ${userMenuOpen ? styles.userSectionOpen : ''}`}>
         <div className={styles.notifBell} onClick={handleNotifClick}>
           <i className="fa fa-bell"></i>
         </div>
         <img className={styles.userAvatar} src="https://i.pinimg.com/736x/e6/50/7f/e6507f42d79520263d8d952633cedcf2.jpg" alt="Anime Avatar"onClick={handleAvatarClick}></img>
+        <div className={styles.nameSection}>
+          <p className={styles.name}>Username</p>
+          <p className={styles.role}>Admin Account</p>
+        </div>
       </div>
+
+      {/* Mobile view */}
+      
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+       </div>
+
+      <div className={styles.mobileDots} onClick={toggleUserMenu}>
+        <span>⋯</span>
+      </div>
+
     </nav>
     <DateBanner className="adminNav"/>
     </>
--- frontend/src/components/navigations/agent-nav/AgentNav.jsx
+++ frontend/src/components/navigations/agent-nav/AgentNav.jsx
@@ -1,5 +1,5 @@
-import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import styles from './agent-nav.module.css';
 
@@ -9,6 +9,7 @@ import NotifModal from '../../modals/notif-action/NotifModal';
 import ProfileModal from '../../modals/profile-action/ProfileModal';
 
 export default function AgentNav() {
+  const location = useLocation();
   const [openProfileModal, setOpenProfileModal] = useState(false);
   const [openNotifModal, setOpenNotifModal] = useState(false);
 
@@ -22,10 +23,37 @@ export default function AgentNav() {
     setOpenProfileModal(false);
   };
 
+  // modal close when the page is resize
+  useEffect(() => {
+    const handleResize = () => {
+      setOpenProfileModal(false); 
+      setOpenNotifModal(false);
+      setUserMenuOpen(false);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // clean event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
+
+  // Burger Menu
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+    setUserMenuOpen(false);
   };
+
+  // Three dots Menu
+  const [userMenuOpen, setUserMenuOpen] = useState(false); 
+  const toggleUserMenu = () => {
+    setUserMenuOpen(prev => !prev);
+    setMenuOpen(false);
+    setOpenProfileModal(false); 
+    setOpenNotifModal(false);
+  };  
 
   return (
     <>
@@ -34,14 +62,11 @@ export default function AgentNav() {
 
       <nav className={styles.navBar}>
         <div className={styles.logoSection}>
-          <div className={styles.logoImg}></div>
-          <span>DTS</span>
-        </div>
-
-        <div className={styles.hamburger} onClick={toggleMenu}>
-          <div className={styles.bar}></div>
-          <div className={styles.bar}></div>
-          <div className={styles.bar}></div>
+          <div className={styles.logoImg}>
+            <img src="/logotixx.png" alt="logo" />
+          </div>
+          <p>TicketFlow</p>
+          {/* <span>TickTrack</span> */}
         </div>
 
         {/* nav-links */}
@@ -81,18 +106,34 @@ export default function AgentNav() {
           </NavLink>
         </div>
 
-        <div className={styles.userSection}>
-          <p>Username</p>
-          <div className={styles.notifBell} onClick={handleNotifClick}>
-            <i className="fa fa-bell"></i>
-          </div>
-          <img
-            className={styles.userAvatar}
-            src="https://i.pinimg.com/736x/e6/50/7f/e6507f42d79520263d8d952633cedcf2.jpg"
-            alt="Anime Avatar"
-            onClick={handleAvatarClick}
-          />
+         <div className={`${styles.userSection} ${userMenuOpen ? styles.userSectionOpen : ''}`}> {/* className={styles.userSection} */}
+            <div className={styles.notifBell} onClick={handleNotifClick}>
+              <i className="fa fa-bell"></i>
+            </div>
+            <img
+              className={styles.userAvatar}
+              src="https://i.pinimg.com/736x/e6/50/7f/e6507f42d79520263d8d952633cedcf2.jpg"
+              alt="Anime Avatar"
+              onClick={handleAvatarClick}
+            />
+            <div className={styles.nameSection}>
+              <p className={styles.name}>Username</p>
+              <p className={styles.role}>Agent Account</p>
+            </div>
+        </div> 
+
+         {/* Mobile view */}
+
+         <div className={styles.hamburger} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
         </div>
+
+        <div className={styles.mobileDots} onClick={toggleUserMenu}>
+          <span>⋯</span>
+        </div>
+
       </nav>
       <DateBanner className="agentNav" />
     </>
