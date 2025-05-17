ea1b9d19045a5354d9963e842fa00fa6caf0eb0c
mrccdric27 <mayuga.marccedricc@gmail.com>
2025-05-17 12:23:37 +0800

    removed __pycache__

--- start_servers.sh
+++ start_servers.sh
@@ -6,8 +6,14 @@ npx json-server --watch db.json --port 5000 &
 cd ../..
 
 # Start Django server
-echo "Starting Django..."
+echo "Starting user_service..."
 cd user_service
+python manage.py runserver &
+cd ..
+
+# Start Django server
+echo "Starting workflow_service..."
+cd workflow_service
 python manage.py runserver &
 cd ..
 
