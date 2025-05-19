c59d88aba6f2cc43b0bb32fe47e00bc0736e7a5c
mrccdrc27 <mayuga.marccedricc@gmail.com>
2025-05-20 00:16:02 +0800

    docker added

--- history_log_service/historylog_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import HistoryLog
-from .serializers import HistoryLogSerializer
-
-class HistoryLogViewSet(viewsets.ModelViewSet):
-    queryset = HistoryLog.objects.all().order_by('-timestamp')
-    serializer_class = HistoryLogSerializer
-
--- workflow_service/positions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('create/', PositionCreateView.as_view(), name='position-create'),
-    path('list/', PositionListView.as_view(), name='position-list'),  # optional
-]
