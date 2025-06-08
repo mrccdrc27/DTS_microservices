0351869c146f2aaf97b51abb389d9a659087ebb8
mrccdric27 <mayuga.marccedricc@gmail.com>
2025-06-09 02:24:45 +0800

    feat: Add initial ticket service implementation with Celery integration and ASGI/WGI configurations

--- ticket_service/tickets/views.py
+++ ticket_service/tickets/views.py
@@ -6,10 +6,3 @@ from .serializers import TicketSerializer
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all().order_by('-opened_on')
     serializer_class = TicketSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        ticket = serializer.save()
-        
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
